7d4d5f01e14b6e919fa0a9f82ebdbcaec676006e
Asperger <stkoso0835@gmail.com>
2014-10-10 19:58:44 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,19 +5,19 @@ int main(int argc, char** argv){
 		cout << "Illegal command line" << endl;
 		return 1;
 	}
-  // Declare the functional objects 
-  AlgParser p; 
-  AlgTimer t; 
-  // Pass the arguement 1 as the input file name 
-  p.Parse(argv[1]);
-  // Start timer 
-  t.Begin(); 
-  // Start sorting
-  if (p.sort(argv[3])) cout << "Sorting complete" << endl;
-  else cout << "Illegal algorithm" << endl;
-  // Write the result
-  p.Write(argv[2]);
-  // Display the accumulated time 
-  cout << "The execution spends " << t.End() << " seconds" << endl; 
-  return 1; 
+	// Declare the functional objects 
+	AlgParser p; 
+	AlgTimer t; 
+	// Pass the arguement 1 as the input file name 
+	p.Parse(argv[1]);
+	// Start timer 
+	t.Begin(); 
+	// Start sorting
+	if (p.sort(argv[3])) cout << "Sorting complete" << endl;
+	else cout << "Illegal algorithm" << endl;
+	// Write the result
+	p.Write(argv[2]);
+	// Display the accumulated time 
+	cout << "The execution spends " << t.End() << " seconds" << endl; 
+	return 1; 
 } 
